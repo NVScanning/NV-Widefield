e31f539b08b38191461f5782ead6da8332b8f88a
NVScanning <nvscanning.cpfs@gmail.com>
2026-08-10 11:15:29 +0200

    changed widefield to save only the postprocessed binned measurement if it exists

--- nv_widefield/widefield_odmr.py
+++ nv_widefield/widefield_odmr.py
@@ -30,7 +30,7 @@ def measure_odmr(cam, sg, freqs, dwell, n_windows, n_iter: int = 1) -> np.ndarra
 
     # throw out first scan, it's sometimes fucked
     pci.sweep_freqs_binned_ringBuf(cam, sg, dwell, freqs, n_windows, n_iter * 2, 0 * 2)
-    pci.sweep_freqs_binned_ringBuf(cam, sg, dwell, freqs[::-1], n_windows, n_iter * 2,0 * 2 + 1)[::-1]
+    pci.sweep_freqs_binned_ringBuf(cam, sg, dwell, freqs[::-1], n_windows, n_iter * 2,0 * 2 + 1)
 
     t0 = time.time()
     brightnesses = np.zeros((n_iter*2, image.shape[0], image.shape[1], freqs.size)) # should be n_iter*2 when reversing as well
@@ -51,21 +51,21 @@ def measure_odmr(cam, sg, freqs, dwell, n_windows, n_iter: int = 1) -> np.ndarra
 
 def main():
     # params
-    camera_binning = 1 # built-int pco camera binning, can only be 1,2,4
-    post_processing_binning = 16
-    focus_point_size = 280  # in physical (unbinned) pixels, diameter of circle of laser point
-    focus_point_centre_x, focus_point_centre_y = 1070,1200  # in pixels, center of the laser point
+    camera_binning = 4 # built-int pco camera binning, can only be 1,2,4
+    post_processing_binning = 8
+    focus_point_size = 1024  # in physical (unbinned) pixels, diameter of circle of laser point
+    focus_point_centre_x, focus_point_centre_y = 1024,1250  # in pixels, center of the laser point
     # TODO: maybe make use of 2D-gaussian to determine centre of focus point automatically
-    n_windows_per_point = 3 # n readouts to increase certainty without overexposing
+    n_windows_per_point = 10 # n readouts to increase certainty without overexposing
     amp_dbm = -10 # from -30 to -10 work, higher gets more contrast but risks RF coupling, Amp at 28V
     dwell =  0.01 # seconds - time between setting a frequency on fn generator and reading value
-    n_iter = 3 # integer >=1
+    n_iter = 20 # integer >=1
     # frequency parameters
     f_center = 2.87e9 # Hz, generally near 2.87GHz
-    span = 0.6e9 # Hz, range of frequencies to sample
-    N = 601 # num points in the frequency space to sample
+    span = 0.2e9 # Hz, range of frequencies to sample
+    N = 401 # num points in the frequency space to sample
 
-    max_peaks = 6
+    max_peaks = 8
 
     roi, x_space, y_space = pci.get_spacial_params(camera_binning,(focus_point_size, focus_point_centre_x, focus_point_centre_y))
     # roi=(1,1,pci.camera_resolution//camera_binning,pci.camera_resolution//camera_binning)
@@ -81,22 +81,24 @@ def main():
     if len(x_space) % post_processing_binning != 0:
         raise ValueError("postprocessing binning is not a divisor of the focus point size after camera binning")
 
-    counts_2D, prev_path = pci.run_odmr_measurement((roi, camera_binning, 0.1), amp_dbm, measure_odmr, (freqs, dwell, n_windows_per_point, n_iter))
+    counts_2D, prev_path = pci.run_odmr_measurement((roi, camera_binning, 0.005), amp_dbm, measure_odmr, (freqs, dwell, n_windows_per_point, n_iter))
 
-    print("Sweeping done")
+    print("Frequency sweeping done")
     if post_processing_binning > 1:
         print(f"now binning {post_processing_binning}x{post_processing_binning} area and converting odmrs to B deltas, estimate time to completion ~{len(x_space)*len(y_space)/post_processing_binning**2}s")
         binned_counts, x_binned, y_binned = pci.bin_counts(counts_2D, post_processing_binning, x_space, y_space)
+        prev_path = oPlot.overwrite_2D_odmr_measurement(x_binned, y_binned, freqs, binned_counts, prev_path, False)
         B_Z_binned, _ = Lfit.counts_to_B_Z(x_binned, y_binned, binned_counts, freqs, max_peaks=max_peaks)
         print("Conversion done, saving and plotting")
         oPlot.save_2D_odmr_measurement(x_binned, y_binned, freqs, B_Z_binned, binned_counts)
         oPlot.plot_magnet_image(x_binned, y_binned, B_Z_binned)
-
-    # print(f"now converting raw odmrs to B deltas, estimate time to completion ~{len(x_space)*len(y_space)}s")
-    # B_Z_overall, _ = Lfit.counts_to_B_Z(x_space, y_space, counts_2D, freqs, max_peaks=max_peaks)
-    # print("Conversion done, saving and plotting")
-    # oPlot.save_2D_odmr_measurement(x_space, y_space, freqs, B_Z_overall, counts_2D)
-    # oPlot.plot_magnet_image(x_space, y_space, B_Z_overall)
+    else:
+        prev_path = oPlot.overwrite_2D_odmr_measurement(x_space, y_space, freqs, counts_2D, prev_path, False)
+        print(f"now converting raw odmrs to B deltas, estimate time to completion ~{len(x_space)*len(y_space)}s")
+        B_Z_overall, _ = Lfit.counts_to_B_Z(x_space, y_space, counts_2D, freqs, max_peaks=max_peaks)
+        print("Conversion done, saving and plotting")
+        oPlot.save_2D_odmr_measurement(x_space, y_space, freqs, B_Z_overall, counts_2D)
+        oPlot.plot_magnet_image(x_space, y_space, B_Z_overall)
 
 
 
